12698db9867e76eae8de0b3f52a309f538698cf8
lucasnike <lucas4cristal@gmail.com>
2021-10-12 23:46:21 -0300

    Inittial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from './services/axios'
 
 function App() {
+
+  axios.get('https://pokeapi.co/api/v2/pokemon/')
+    .then((result) => {
+      console.log(result.data);
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello world</h1>
   );
 }
 
